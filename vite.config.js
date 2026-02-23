297b664d6bbec1cfc747637d8e60025607bc5a37
srinivasulu-andra <srinivasuluandra17@gmail.com>
2026-02-23 23:46:05 +0530

    fix project structure: move index.html and netlify.toml to root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/srinu-portfolio/",   // 👈 IMPORTANT
   plugins: [react()],
 })
